66b26e0978fa420a0960887f734fc84df475e7df
francisco <franrogel2003@gmail.com>
2025-10-23 02:22:06 +0000

    seeders carreras

---
+++ database/seeders/CarrerasICPASeeder2.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class CarrerasICPASeeder2 extends Seeder
+{
+    public function run()
+    {
+        $carreras = [
+            [
+                'nombre' => 'Licenciatura en Biología',
+                'instituto_id' => 1,
+                'modalidad' => 'presencial',
+                'sede' => 'Ushuaia',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'nombre' => 'Licenciatura en Ciencias Ambientales',
+                'instituto_id' => 1,
+                'modalidad' => 'presencial',
+                'sede' => 'Ushuaia',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'nombre' => 'Licenciatura en Geología',
+                'instituto_id' => 1,
+                'modalidad' => 'presencial',
+                'sede' => 'Ushuaia',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'nombre' => 'Ingeniería en Agroecología',
+                'instituto_id' => 1,
+                'modalidad' => 'presencial',
+                'sede' => 'Rio Grande',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
+
+        DB::table('carreras')->insert($carreras);
+    }
+}
+
