bf658b0a9c1ef0b84643f79c552e479a028b9f89
frogel <franrogel2003@gmail.com>
2025-11-05 13:01:52 +0000

    seeder_cargos_para_docentes

---
+++ database/seeders/CargosSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cargo;
+use App\Models\Docente;
+use App\Models\Dedicacion;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CargosSeeder extends Seeder
+{
+    public function run()
+    {
+        $cargosDisponibles = [
+            'Titular',
+            'Asociado',
+            'Adjunto',
+            'Jefe de Trabajos Practicos',
+            'Ayudante de Primera'
+        ];
+
+        $docentes = Docente::all();
+
+        foreach ($docentes as $docente) {
+
+            if ($docente->modalidad_desempeño === 'Desarrollo') {
+                $dedicaciones = Dedicacion::whereIn('nombre', ['Simple', 'SemiExclusiva(DP)'])->get();
+            } elseif ($docente->modalidad_desempeño === 'Investigador') {
+                $dedicaciones = Dedicacion::whereIn('nombre', ['SemiExclusiva(DI)', 'Exclusiva'])->get();
+            } 
+
+            $dedicacion = $dedicaciones->random();
+
+
+            Cargo::create([
+                'nombre' => $cargosDisponibles[array_rand($cargosDisponibles)],
+                'dedicacion_id' => $dedicacion->id,
+                'nro_materias_asig' => 0,
+                'sum_horas_frente_aula' => 0,
+                'docente_id' => $docente->id,
+            ]);
+        }
+    }
+}
