0d1bb4a69feaacb8032b6d0f6705c11ad1a5353d
aupi <joaquin.exequiel.tulian@gmail.com>
2025-11-27 20:23:48 +0000

    arreglitos

--- database/seeders/RolesYPermisosSeeder.php
+++ database/seeders/RolesYPermisosSeeder.php
@@ -79,7 +79,28 @@ class RolesYPermisosSeeder extends Seeder
             // syncPermissions asegurará que solo se asignen los permisos definidos aquí.
             $role->syncPermissions($permisosRol);
         }
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@domain.com'],
+            [
+                'name' => 'admin',
+                'password' => Hash::make('admin123'),
+                'dni' => '00000000',
+                'nombre' => 'Administrador',
+                'apellido' => 'Sistema',
+                'is_activo' => true,
+                'cargo' => 'Administrador',
+                'instituto_id' => null,
+            ]
+        );
+
+        $admin->assignRole('Admin');
 
 
     }
+
+    
+
+
+
+
 }
